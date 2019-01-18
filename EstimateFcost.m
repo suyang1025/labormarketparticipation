61b9c51ab3201ebaaa7905cd84dbdc66895c653c
Yang S <suyang1025@gmail.com>
2019-01-17 21:59:28 -0800

    functions for calibration

--- EstimateFcost.m
+++ EstimateFcost.m
@@ -22,3 +22,22 @@ disp(xMin)
 TimeE = cputime;
 disp('Time Spent (mins)')
 disp((TimeE - TimeS)/60)
+
+
+% Estimation
+HoursWorked35to50 = 0.412;
+LforceParticipationRate2454 = 0.829;
+data = [LforceParticipationRate2454, HoursWorked35to50]
+x0            = [0.0001, 0.9];
+options=optimset('Display','final','MaxFunEvals',10000,'MaxIter',10000,'tolx',0.00001,'tolfun', 1); 
+  % Option on display of output, # of evaluation and convergence criteria
+[xMin,fval]   = fminsearch(@finddistance,x0,options); 
+
+% Display results
+disp('Estimated params with population data')
+disp('alpha, fcost:')
+disp(xMin) 
+% Display time spent 
+TimeE = cputime;
+disp('Time Spent (mins)')
+disp((TimeE - TimeS)/60)
---
+++ finddistance.m
@@ -0,0 +1,7 @@
+function F = finddistance(x)
+alpha = x(1);
+fcost = x(2);
+main_withbounds;
+data = [0.412, 0.829];
+F = WeightedDist(simMoments, data);
+end
